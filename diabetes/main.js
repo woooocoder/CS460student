52ef4bea2e821feea8c30a0de95f335ec95ee8f3
woooocoder <woodyr0416@gmail.com>
2024-11-22 20:17:07 -0500

    added test gltf

--- diabetes/main.js
+++ diabetes/main.js
@@ -1,6 +1,11 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.156.0/build/three.module.js';
+// import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.156.0/examples/jsm/loaders/GLTFLoader.js';
+
+
+
 import { initPane } from './gui.js';
 
+// console.log('GLTF: ', GLTFLoader)
 // Scene
 const scene = new THREE.Scene();
 // Camera
@@ -10,19 +15,25 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-// Cube
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-// Animation loop
+scene.add(cube); 
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-// Handle resize
+
+// const loader = new GLTFLoader().setPath('./public')
+// loader.load('test.gltf', (gltf) => {
+//     const mesh = gltf.scene
+//     mesh.position.set(0,15,-1)
+//     scene.add(mesh)
+// })
+
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
